de0ffdf3f2e39f42c1bab4b8d3606f4e7264e2a2
Sanjay <sanzaikc@gmail.com>
2022-08-04 17:35:35 +0545

    mend

--- frontend/src/redux/services/formService.js
+++ frontend/src/redux/services/formService.js
@@ -2,9 +2,21 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const formApi = createApi({
   reducerPath: "formApi",
+  tagTypes: ["Form"],
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/" }),
   endpoints: (builder) => ({
-    getAllForms: () => "/forms",
+    getAllForms: builder.query({
+      query: () => "/forms",
+
+      providesTags: (result) => {
+        return result
+          ? [
+              ...result.map(({ id }) => ({ type: "Form", id })),
+              { type: "Form", id: "LIST" },
+            ]
+          : [{ type: "Form", id: "LIST" }];
+      },
+    }),
   }),
 });
 
