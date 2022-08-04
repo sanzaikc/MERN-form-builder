989fdb349e9439f846a07b2e6b07075fb50dce00
Sanjay <sanzaikc@gmail.com>
2022-08-04 09:52:58 +0545

    mend

--- frontend/src/components/DropZone.js
+++ frontend/src/components/DropZone.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useDrop } from "react-dnd";
 
-import { Box, Paper } from "@mui/material";
+import { Box, Button, Paper } from "@mui/material";
 import { ItemTypes } from "../utils/dndItemTypes";
 
 export const DropZone = () => {
@@ -10,11 +10,20 @@ export const DropZone = () => {
 
   const [{ isOver }, drop] = useDrop({
     accept: ItemTypes.INPUT,
-    drop: (item, moniter) => setFormElements([...formElements, { ...item }]),
+    drop: (item, moniter) => handleAddInputField(item),
     collect: (moniter) => ({
       isOver: !!moniter.isOver(),
     }),
   });
+
+  const handleAddInputField = (inputField) => {
+    const alreadyInForm = !!formElements.find(
+      (el) => el.type === inputField.type
+    );
+    if (alreadyInForm) return;
+
+    setFormElements([...formElements, { ...inputField }]);
+  };
 
   return (
     <Paper>
@@ -26,6 +35,15 @@ export const DropZone = () => {
           borderStyle: isOver ? "dashed" : "",
         }}
       >
+        <Box display="flex" justifyContent="flex-end">
+          <Button
+            size="small"
+            variant="contained"
+            onClick={() => setFormElements([])}
+          >
+            Reset
+          </Button>
+        </Box>
         <p>{JSON.stringify(formElements)}</p>
       </Box>
     </Paper>
