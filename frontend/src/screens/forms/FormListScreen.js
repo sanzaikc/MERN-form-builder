ea4f02d5367b8632b0e7d529e67291fe190c026a
Sanjay <sanzaikc@gmail.com>
2022-08-05 10:21:48 +0545

    Form card as separate component

--- frontend/src/screens/forms/FormListScreen.js
+++ frontend/src/screens/forms/FormListScreen.js
@@ -1,40 +1,10 @@
 import React from "react";
 
-import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
 
-import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
-import FeedIcon from "@mui/icons-material/Feed";
+import { FormCard } from "../../components/FormCard";
 
 import { useGetAllFormsQuery } from "../../redux/services/formService";
-
-const FormCard = ({ form }) => {
-  const navigate = useNavigate();
-
-  return (
-    <Grid item xs={12} sm={6} md={4} lg={2} xl={1.5}>
-      <Card
-        variant="outlined"
-        style={{
-          cursor: "pointer",
-        }}
-        onClick={() => navigate(`/forms/${form._id}/edit`)}
-      >
-        <CardContent>
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <FeedIcon
-              style={{
-                height: 150,
-                width: 100,
-                color: "lightgray",
-              }}
-            />
-            <Typography variant="body1">{form.name}</Typography>
-          </Box>
-        </CardContent>
-      </Card>
-    </Grid>
-  );
-};
 
 export const FormListScreen = () => {
   const { data: formList, isLoading } = useGetAllFormsQuery();
