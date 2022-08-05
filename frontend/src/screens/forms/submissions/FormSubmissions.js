739c8d23b6f0e233f0ae15a65c1b3a7b6c05986e
Sanjay <sanzaikc@gmail.com>
2022-08-05 17:35:07 +0545

    mend

--- frontend/src/screens/forms/submissions/FormSubmissions.js
+++ frontend/src/screens/forms/submissions/FormSubmissions.js
@@ -1,5 +1,10 @@
+import { Grid } from "@mui/material";
 import React from "react";
 
 export const FormSubmissions = () => {
-  return <div>Form Submissions</div>;
+  return (
+    <Grid container gap={4}>
+      Form Submissions
+    </Grid>
+  );
 };
